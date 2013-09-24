918221744c164b4ccc00d07c50ee87138b75b538
Ahmed Azaan <azaan@outlook.com>
2013-09-24 19:26:00 +0530

    add working script

---
+++ src/cli.py
@@ -0,0 +1,95 @@
+"""
+This module is the CLI interface for the converter
+
+Author: Ahmed Azaan Hassan (azaan@outlook.com) 2013
+        https://github.com/aeonaxan/
+"""
+
+import sys
+import helpers
+
+def main():
+    if len(sys.argv) != 3:
+        printHelp(sys.argv[0])
+        sys.exit(1)
+
+    if not helpers.is_repository():
+        print "Must be a git repository"
+        sys.exit(2)
+
+    remote_name = sys.argv[1]
+    protocol = sys.argv[2]
+
+    remotes = helpers.get_remotes()
+    remote = helpers.get_remote(sys.argv[1], remotes)
+
+    if remote == None:
+        print "Remote {0} does not exists. Valid remotes are:".format(remote_name)
+        helpers.print_remotes(remotes)
+        sys.exit(3)
+
+    if protocol == "ssh":
+        convertSSH(remote)
+    elif protocol == "https":
+        convertHTTPS(remote)
+    else:
+        print "Valid protocols are 'https' and 'ssh'"
+        sys.exit(5)
+
+
+def convertSSH(remote):
+    """Converts the remote's url to a ssh url"""
+    if not helpers.is_https_url(remote[1]):
+        if helpers.is_ssh_url(remote[1]):
+            print "Remote {0} is already a Github ssh URL".format(remote[0])
+        else:
+            print "Unrecognized URL for remote. Please see help"
+            print "{0} - {1}".format(remote[0], remote[1])
+
+        return
+
+    url_fragments = remote[1].lower().strip('.git').split('/')
+    assert len(url_fragments) == 5, "Invalid Github url " + remote[1]
+
+    url = "git@github.com:" + url_fragments[3] + '/' + url_fragments[4] + '.git'
+
+    if helpers.set_git_remote_url(remote[0], url):
+        print "Successfully changed {0} URL to [{1}]".format(remote[0], url)
+    else:
+        print "Unexpected error! could not change the URL"
+
+
+
+def convertHTTPS(remote):
+    """Converts the remote's url to a ssh url"""
+    if not helpers.is_ssh_url(remote[1]):
+        if helpers.is_https_url(remote[1]):
+            print "Remote {0} is already a Github ssh URL".format(remote[0])
+        else:
+            print "Unrecognized URL for remote. Please see help"
+            print "{0} - {1}".format(remote[0], remote[1])
+
+        return
+
+    url_fragments = remote[1].lower().strip('.git').split(':')
+    assert len(url_fragments) == 2, "Invalid github url " + remote[1]
+
+    url = "https://github.com/" + url_fragments[1]
+
+    if helpers.set_git_remote_url(remote[0], url):
+        print "Successfully changed {0} URL to [{1}]".format(remote[0], url)
+    else:
+        print "Unexpected error! could not change the URL"
+
+
+def printHelp(name):
+    """Prints the usage/help message for the script"""
+    print "Usage:"
+    print "\t{0} <remote_name> <https|ssh protocol to change into>".format(name)
+    print "\n\tPlease note, this will only work with valid Github URLs"
+    print "\tGithub SSH  : git@github.com:username/project.git"
+    print "\tGithub HTTPS: https://github.com/username/project"
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/helpers.py
@@ -0,0 +1,68 @@
+import subprocess
+
+def is_repository():
+    with open('/dev/null', 'w') as f:
+        res = subprocess.call(["git", "rev-parse"], stdout=f, stderr=f)
+        return res == 0
+
+
+def set_git_remote_url(remote, url):
+    """Sets the git remote url for the given remote
+    returns whether the operation succeded"""
+
+    with open('/dev/null', 'w') as f:
+        res = subprocess.call(["git", "remote", "set-url", remote, url], stdout=f, stderr=f)
+        return res == 0
+
+def get_remotes():
+    """Returns a list of remotes and their url's. If there are two urls
+    for push and fetch, only one of it will be returned, we assume they
+    are the same"""
+
+    p = subprocess.Popen(["git", "remote", "-v"], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+
+    if err is not None:
+        print "Unexpected error in getting Git remotes (git remove -v)"
+        raise err
+
+    remotes = []
+    remote_names = []
+    for remote in out.split("\n"):
+        if len(remote) == 0:
+            continue
+
+        remote = tuple(remote.split()[:2])
+
+        if remote[0] in remote_names:
+            continue
+
+        remotes.append(remote)
+        remote_names.append(remote[0])
+
+    return remotes
+
+
+def get_remote(remote, remotes):
+    """Returns the remote if it exists, else None"""
+    for r in remotes:
+        if r[0] == remote:
+            return r
+    return None
+
+
+def print_remotes(remotes):
+    """Prints a list of the given remotes and url"""
+    for remote in remotes:
+        print "{0} - {1}".format(remote[0], remote[1])
+
+
+def is_ssh_url(url):
+    """Checks whether the github url is a ssh url"""
+    # make strict assumptions about the url, better to fail rather break a remote
+    return url.find("git@github.com:") == 0
+
+def is_https_url(url):
+    """Checks whether the github url is a https url"""
+    # make strict assumptions about the url, better to fail rather break a remote
+    return url.find("https://github.com/") == 0
