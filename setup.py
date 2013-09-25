40896137b434ae9c0ef888d70e4dfe25b27ecc6b
Ahmed Azaan <azaan@outlook.com>
2013-09-26 01:55:18 +0530

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "Github-url-converter",
-    version = "0.0.1",
+    version = "0.1.1",
     author = "Ahmed Azaan",
     author_email = "azaan@outlook.com",
     description = ("Quick and easy way to convert Github remote url's from HTTPS"
@@ -23,7 +21,7 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 2.7",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
     ],
